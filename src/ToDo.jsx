6ccfef85f8b36baeaf16af87269f8014301c1e6d
Ronald Stiven Nastacuas Gallardo <ronald.nastacuas@vasslatam.com>
2025-06-01 18:32:46 -0500

    first commit

---
+++ src/ToDo.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function ToDo() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default ToDo
